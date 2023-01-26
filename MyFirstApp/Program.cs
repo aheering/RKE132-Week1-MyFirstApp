5c74c0a36f3d595938de168c25363547a6982e51
aheering <heering.agnes@gmail.com>
2023-01-26 19:43:17 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Rakendus palub kasutajal sisestada oma nimi
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output - väljund
+//string - sõne
+string userName = Console.ReadLine(); //Input - sisend
+
+Console.WriteLine("Hello, " + userName + "!");
+
+// string interpolation
+Console.WriteLine($"Hello, {userName}!");
